1c5054006e795e936253f98e1124a45d0dcc9d9b
Tristan Owen <tristan.owen@btinternet.com>
2020-06-14 13:13:18 +0100

    Made all of src and main comply to pep8

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # blackFriday
 predicting black friday sales purchases
+
+To run flake8 on code ignoring errors I dont like such as line lenght being uner 79 etc run the following command in blackFriday directory:
+flake8 --max-line-length=120 --ignore=E402,F841,F401,E302,E305,F403,F405 .\src\
--- src/conf.py
+++ src/conf.py
@@ -2,9 +2,10 @@ train_path = "train.csv"
 test_path = "test.csv"
 
 num_to_cat = {True: ["Gender", "Age", "Occupation", "City_Category", "Stay_In_Current_City_Years", "Marital_Status",
-                   "Product_Category_1", "Product_Category_2", "Product_Category_3", "num_of_cats", "two_cats", "three_cats"],
+                     "Product_Category_1", "Product_Category_2", "Product_Category_3", "num_of_cats", "two_cats",
+                     "three_cats"],
               False: ["Gender", "Age", "Occupation", "City_Category", "Stay_In_Current_City_Years", "Marital_Status",
-                   "Product_Category_1", "Product_Category_2", "Product_Category_3"]}
+                      "Product_Category_1", "Product_Category_2", "Product_Category_3"]}
 
 ordinal_cols = ["Age", "Stay_In_Current_City_Years"]
 one_hot_list = {True: ["Gender", "Occupation", "City_Category", "Marital_Status"],
